3cb071268bdce6494a60d29cb9fe164df93844cd
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-04-04 19:16:14 +0100

    Refactor audio player

--- src/AudioContext.jsx
+++ src/AudioContext.jsx
@@ -10,35 +10,7 @@ const AudioContext = createContext();
 
 export const AudioProvider = ({ children }) => {
   const audioRef = useRef(new Audio());
-  const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
-  const [currentTime, setCurrentTime] = useState("--:--");
-
-  const togglePlayPause = () => {
-    const audio = audioRef.current;
-    if (audioRef.current) {
-      audioRef.current.muted = false; // Unmute on interaction
-      audioRef.current.play().catch(console.error);
-      setIsPlaying(!audioRef.current.paused);
-    }
-
-    if (audio) {
-      const playPromise = audio.play();
-
-      if (playPromise !== undefined) {
-        playPromise
-          .then(() => {
-            setIsPlaying(true);
-            console.log("Playback started");
-          })
-          .catch((error) => {
-            console.error("Playback prevented:", error);
-          });
-      } else {
-        console.warn("No play promise available");
-      }
-    }
-  };
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -46,7 +18,6 @@ export const AudioProvider = ({ children }) => {
     const updateProgress = () => {
       if (audio.duration) {
         setProgress((audio.currentTime / audio.duration) * 100);
-        setCurrentTime(formatTime(audio.currentTime));
       }
     };
 
@@ -54,23 +25,12 @@ export const AudioProvider = ({ children }) => {
     return () => audio.removeEventListener("timeupdate", updateProgress);
   }, []);
 
-  const formatTime = (seconds) => {
-    const mins = Math.floor(seconds / 60);
-    const secs = Math.floor(seconds % 60);
-    return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
-  };
-
   return (
     <AudioContext.Provider
       value={{
         audioRef,
-        isPlaying,
-        setIsPlaying,
         progress,
         setProgress,
-        currentTime,
-        setCurrentTime,
-        togglePlayPause,
       }}
     >
       {children}
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { useAudio } from "../AudioContext";
+
+export const Timeline = ({ playingGuest }) => {
+  const { chapters } = playingGuest;
+  const [hoveredChapter, setHoveredChapter] = useState("");
+  const audioContext = useAudio();
+  const { audioRef, progress, setProgress } = audioContext;
+  const handleChapterClick = (chapterStartTime, event) => {
+    event.stopPropagation();
+    if (audioRef.current) {
+      audioRef.current.currentTime = chapterStartTime;
+      setProgress((chapterStartTime / audioRef.current.duration) * 100);
+    }
+  };
+  return (
+    <>
+      <div
+        className="completed-progress__desktop"
+        style={{ height: `${progress}%` }}
+      />
+
+      {chapters.map((chapter, index) => (
+        <React.Fragment key={index}>
+          <div
+            onMouseEnter={() => {
+              setHoveredChapter(chapter?.title);
+            }}
+            onMouseLeave={() => setHoveredChapter("")}
+            className="progress-bar-chapter-slit-hitbox__desktop"
+            style={{
+              top: `${
+                audioRef.current?.duration
+                  ? (chapter.startTime / audioRef.current.duration) * 100 - 1
+                  : 0
+              }%`,
+            }}
+            onClick={(e) => handleChapterClick(chapter.startTime, e)}
+          />
+
+          <div
+            className="progress-bar-chapter-slit__desktop"
+            style={{
+              height: hoveredChapter === chapter?.title ? "3px" : "1px",
+              top: `${
+                audioRef.current?.duration
+                  ? (chapter.startTime / audioRef.current.duration) * 100
+                  : 0
+              }%`,
+              backgroundColor:
+                hoveredChapter === chapter?.title ? "#000000" : "white",
+            }}
+            onMouseEnter={() => setHoveredChapter(chapter?.title)}
+            onMouseLeave={() => setHoveredChapter("")}
+          />
+        </React.Fragment>
+      ))}
+      <img
+        src="/indicator.png"
+        className="progress-bar-current-icon__desktop"
+        alt="Indicator"
+      />
+    </>
+  );
+};
