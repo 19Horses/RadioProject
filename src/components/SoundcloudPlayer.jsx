b759cba2e9272ecc18886fe101fdd104781ce19d
Fesoshe Olunaike <w3by0g1@proton.me>
2025-07-25 13:00:01 +0100

    better play button

--- src/components/SoundcloudPlayer.jsx
+++ src/components/SoundcloudPlayer.jsx
@@ -201,16 +201,21 @@ export default function SoundCloudPlayer({ playingGuest, isMobile }) {
   };
 
   const formatTime = (seconds) => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
+    if (isNaN(seconds) || seconds == null || seconds === Infinity) {
+      return "--:--";
+    }
+
+    const hrs = Math.floor(seconds / 3600);
+    const mins = Math.floor((seconds % 3600) / 60);
     const secs = Math.floor(seconds % 60);
 
-    if (hours > 0) {
-      return `${hours}:${minutes < 10 ? "0" : ""}${minutes}:${
-        secs < 10 ? "0" : ""
-      }${secs}`;
+    if (hrs > 0) {
+      return `${hrs}:${mins.toString().padStart(2, "0")}:${secs
+        .toString()
+        .padStart(2, "0")}`;
+    } else {
+      return `${mins}:${secs.toString().padStart(2, "0")}`;
     }
-    return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
   };
 
   useEffect(() => {
