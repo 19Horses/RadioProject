3c3567dc36d82013a824bfd16a3461a5f6606954
Fesoshe Olunaike <w3by0g1@proton.me>
2025-08-05 20:45:36 +0100

    mobile aspect ratio instead

--- src/pages/rphead.jsx
+++ src/pages/rphead.jsx
@@ -23,8 +23,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function RPHead({ isMobile }) {
   const [canvasSize, setCanvasSize] = useState({
-    width: isMobile ? 540 : 640,
-    height: isMobile ? 960 : 480,
+    width: isMobile ? 640 : 640,
+    height: isMobile ? 640 : 480,
   });
 
   const canvasContainerRef = useRef(null);
